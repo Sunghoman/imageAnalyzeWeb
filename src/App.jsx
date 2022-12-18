e035c12fff7e4b7773fae0052c2b07ddba192614
Sunghoman <ksh_1035@naver.com>
2022-12-18 23:41:27 +0900

    이미지 분석 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,89 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect, useRef, useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
+import * as mobilenet from '@tensorflow-models/mobilenet';
+import * as tf from '@tensorflow/tfjs';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [imgFile, setImgFile] = useState();
+  const [result, setResult] = useState();
+  const imageInput = useRef();
+
+  const handleClick = () => {
+    imageInput.current.click();
+  }
+
+  const previewImage = () => {
+    const file = imageInput.current.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = () => {
+      setImgFile(reader.result);
+    };
+  };
+
+  const analyzeImage = () => {
+    const img = document.getElementById('imagePreview')
+    mobilenet.load().then((model) => {
+      model.classify(img).then((predictions) => {
+        console.log(predictions);
+        setResult(predictions);
+      })
+    })
+  }
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+      <div 
+        className='previewContainer'
+        ref={imageInput}
+        onClick={handleClick}
+      >
+        <img 
+          id='imagePreview'
+          className='imagePreview'
+          src={imgFile ? imgFile : 'src/assets/picture.png'}
+        />
       </div>
-      <h1>Vite + React</h1>asd
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <h1>동물 품종 알려줌</h1>
+      <h4>이미지 업로드하면 대충 알아맞힘</h4>
+
+      <input 
+        type="file"
+        accept="image/*"
+        ref={imageInput}
+        style={{display: 'none'}} 
+        onChange={previewImage}
+      />
+
+      <button 
+        className="btn-upload"
+        onClick={analyzeImage}
+      >
+        <span>UPLOAD</span>
+        <div className="progress"></div>
+        <div className="check">
+          <span></span>
+          <span></span>
+        </div>
+      </button>
+
+      <div className='result'>
+        {
+          result ? (
+            <>
+              <h3>이거... 혹시</h3>
+              <h2>{result[0]?.className}</h2>
+              <span>인듯</span>
+              <h2>{Math.round(result[0].probability * 100)}% 정도 비슷하네여</h2>
+            </>
+          ) : null
+        }
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </div>
   )
 }
